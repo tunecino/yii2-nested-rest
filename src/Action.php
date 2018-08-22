cc3c14f091ed9bc5d649a871c7a03291b8981af1
Roman Kulikov <r.v.kulikov@yandex.ru>
2018-08-22 17:28:47 +0300

    Action fixed counting error "count(): Parameter must be an array or an object that implements Countable"

--- src/Action.php
+++ src/Action.php
@@ -1,21 +1,25 @@
 <?php
 /**
- * @link https://github.com/tunecino/yii2-nested-rest
+ * @link      https://github.com/tunecino/yii2-nested-rest
  * @copyright Copyright (c) 2016 Salem Ouerdani
- * @license http://opensource.org/licenses/BSD-3-Clause
+ * @license   http://opensource.org/licenses/BSD-3-Clause
  */
 
 namespace tunecino\nestedrest;
 
 use Yii;
 use yii\base\InvalidConfigException;
-use yii\web\NotFoundHttpException;
+use yii\db\ActiveQueryInterface;
+use yii\db\ActiveRecordInterface;
 use yii\helpers\StringHelper;
+use yii\web\NotFoundHttpException;
 
 /**
  * Action is the base class for nested action classes that depends on the custom UrlRule class.
  *
  * @author Salem Ouerdani <tunecino@gmail.com>
+ *
+ * @property \yii\db\ActiveRecordInterface $relativeModel
  */
 class Action extends \yii\rest\Action
 {
@@ -30,12 +34,12 @@ class Action extends \yii\rest\Action
      */
     protected $relationName;
     /**
-      * @var string name of the attribute name used as a foreign key in the related model. also used to build the 'prefix'.
+     * @var string name of the attribute name used as a foreign key in the related model. also used to build the 'prefix'.
      * This should be provided by the UrlClass within queryParams.
      */
     protected $linkAttribute;
     /**
-      * @var primary key value of the linkAttribute.
+     * @var string primary key value of the linkAttribute.
      * This should be provided by the UrlClass within queryParams.
      * @see linkAttribute
      */
@@ -43,6 +47,7 @@ class Action extends \yii\rest\Action
 
     /**
      * @inheritdoc
+     * @throws InvalidConfigException
      */
     public function init()
     {
@@ -60,10 +65,14 @@ class Action extends \yii\rest\Action
 
     /**
      * Checks if the expected params that should be provided by the custom UrlClass are not missing.
-     * @return Bolean.
+     *
+     * @param $params
+     *
+     * @return bool
      */
-    protected function expectedParams($params) {
-        $expected = ['relativeClass','relationName','linkAttribute'];
+    protected function expectedParams($params)
+    {
+        $expected = ['relativeClass', 'relationName', 'linkAttribute'];
         foreach ($expected as $attr) {
             if (isset($params[$attr]) === false) return false;
             if ($attr === 'linkAttribute' && isset($params[$params[$attr]]) === false) return false;
@@ -73,13 +82,15 @@ class Action extends \yii\rest\Action
 
     /**
      * Finds the related model.
+     *
      * @return \yii\db\ActiveRecordInterface.
      * @throws NotFoundHttpException if not found.
      */
     public function getRelativeModel()
     {
+        /** @var ActiveRecordInterface $relativeClass */
         $relativeClass = $this->relativeClass;
-        $relModel = $relativeClass::findOne($this->relative_id);  
+        $relModel      = $relativeClass::findOne($this->relative_id);
 
         if ($relModel === null)
             throw new NotFoundHttpException(StringHelper::basename($relativeClass) . " '$this->relative_id' not found.");
@@ -93,24 +104,31 @@ class Action extends \yii\rest\Action
     /**
      * Finds the model or the list of models corresponding
      * to the specified primary keys values within the relative model retreived by [[getRelativeModel()]].
+     *
      * @param string $IDs should hold the list of IDs related to the models to be loaded.
-     * it must be a string of the primary keys values separated by commas.
+     *                    it must be a string of the primary keys values separated by commas.
+     *
      * @return \yii\db\ActiveRecordInterface
      * @throws NotFoundHttpException if not found or not related.
      */
     public function findCurrentModels($IDs)
     {
+        /** @var ActiveRecordInterface $modelClass */
         $modelClass = $this->modelClass;
-        $pk = $modelClass::primaryKey()[0];
-        $ids = preg_split('/\s*,\s*/', $IDs, -1, PREG_SPLIT_NO_EMPTY);
-        $getter = 'get' . $this->relationName;
+        $pk         = $modelClass::primaryKey()[0];
+        $ids        = preg_split('/\s*,\s*/', $IDs, -1, PREG_SPLIT_NO_EMPTY);
+        $getter     = 'get' . $this->relationName;
 
         $relModel = $this->getRelativeModel();
-        
-        $q = $relModel->$getter()->where([$pk => $ids]);
+
+        /** @var ActiveQueryInterface $q */
+        $q     = $relModel->$getter()->where([$pk => $ids]);
         $model = count($ids) > 1 ? $q->all() : $q->one();
 
-        if ($model === null or count($model) !== count($ids)) 
+        $ci = count($ids);
+        $cm = $ci > 1 ? count($model) : count([$model]);
+
+        if ($model === null or $cm !== $ci)
             throw new NotFoundHttpException("Not found or unrelated objects.");
 
         return $model;
